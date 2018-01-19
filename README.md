857a62acdcbd370628b1a44170f39721032eb379
Unmesh Gundecha <upgundecha@gmail.com>
2018-01-19 13:44:17 +0530

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Custom Alexa Skill
+
+## Intent Schema
+
+```
+{
+    "intents":[
+        {
+            "intent":"FollowerCount"
+        }
+    ]
+}
+```
+## Utterances
+```
+FollowerCount how many followers do I have
+FollowerCount current followers
+FollowerCount number of followers
+```
+
+
---
+++ index.js
@@ -0,0 +1,53 @@
+var request = require('request');
+
+exports.handler = (event, context, callback) => {
+  try {
+    switch (event.request.type) {
+      case "LaunchRequest":
+        buildResponse("Welcome to Github")
+        break;
+      case "IntentRequest":
+        switch(event.request.intent.name){
+          case "FollowerCount":
+            getFollowerCount(function(count){
+              context.succeed(buildResponse(`You have ${count} followers`))
+            })
+            break;
+          default:
+          context.succeed(buildResponse("Sorry I couldn't understand"))
+        }
+        break;
+      case "SessionEndedRequest":
+        break;
+      default:
+        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
+    }
+  } catch(error) { context.fail(`Exception: ${error}`) }
+}
+
+getFollowerCount = (callback) =>{
+  var options = {
+    url: `https://api.github.com/users/${process.env.USERNAME}`,
+    headers: {
+      'User-Agent': 'alexa-skill'
+    }
+  };
+  request(options, function(error, response, body){
+    var data = JSON.parse(body)
+    callback(data.followers)
+  })
+}
+
+buildResponse = (outputText) => {
+  return {
+    version: "1.0",
+    sessionAttributes: {},
+    response: {
+      outputSpeech: {
+        type: "PlainText",
+        text: outputText
+      }
+    },
+      shouldEndSession: true
+  }
+}
